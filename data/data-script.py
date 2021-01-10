c34983f80d6c175fd9a1a705078e62bf68fb0e5d
Vi Thai <qvthai@sfu.ca>
2021-01-09 23:30:34 -0800

    Completed data-script and added all the data from datasets to Firestore.

--- data/data-script.py
+++ data/data-script.py
@@ -1,28 +1,33 @@
-#import firebase_admin
-#from firebase_admin import credentials
-#from firebase_admin import firestore
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
 import pandas as pd
 
-#cred = credentials.Certificate('grey-movie-firebase-adminsdk-9561z-8dadf14839.json')
+cred = credentials.Certificate('grey-movie-firebase-adminsdk-9561z-8dadf14839.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
-
-# Get data from movies.csv
+# Get data from movies.csv and add them to Firestore
 filename = 'movies.csv'
 movies = pd.read_csv(filename, sep = ',')
 size = len(movies.index)
 for i in range(size):
-    print(movies.loc[i,'movieId'])
-    print(movies.loc[i,'title'])
+    movieId = str(movies.loc[i,'movieId'])
     genres = movies.loc[i,'genres'].split('|')
-    for genre in genres:
-        print(genre)
+    data = {
+        'title' : movies.loc[i,'title'],
+        'genres': genres
+    }
+    db.collection('movies').document(movieId).set(data)
 
-# Get data from ratings.csv
+# Get data from ratings.csv and add them to Firestore
 filename = 'ratings.csv'
 ratings = pd.read_csv(filename, sep = ',')
 avg_ratings = ratings.groupby('movieId', as_index = False)['rating'].mean()
 size = len(avg_ratings.index)
 for i in range(size):
-    print(avg_ratings.loc[i,'movieId'])
-    print(avg_ratings.loc[i,'rating'])
+    movieId = str(avg_ratings.loc[i,'movieId'])
+    db.collection('movies').document(movieId).update({
+        'rating': avg_ratings.loc[i,'rating']
+    })
